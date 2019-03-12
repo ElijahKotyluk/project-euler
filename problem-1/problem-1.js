2fc33a5e7837e0fdcf0cb4523e360c90077bd6a2
Eli Kotyluk <eli@riboso.me>
2019-03-12 08:18:10 -0700

    Problem-1: Multiples of 3 & 5

--- problem-1/problem-1.js
+++ problem-1/problem-1.js
@@ -1,6 +1,6 @@
 /*
 **  Multiples of 3 & 5
-** 
+**
 **  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 **  The sum of these multiples is 23.
 **  Find the sum of all the multiples of 3 or 5 below 1000.
@@ -8,13 +8,6 @@
 */
 
 sum = 0;
-
-for(let i = 2; i < 1000; i++) {
-  sum += (i % 3 && i % 5) ? 0 : i
-}
-
-console.log(sum)
-
 i = 2
 
 while(i++, i < 1000) { sum += (i % 3 && i % 5) ? 0 : i }
